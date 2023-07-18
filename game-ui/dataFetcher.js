442048b652cb58a2a17880c8d2930926da6f6a73
ahmedtechm <ahmedtechm2050@gmail.com>
2023-07-18 13:33:10 +0400

    Added JavaScript code to fetch and display player names

--- game-ui/dataFetcher.js
+++ game-ui/dataFetcher.js
@@ -1,12 +1,14 @@
-let lopobj = document.getElementById("lop");
-
-// let newListItem = document.createElement("li");
-// newListItem.textContent = "Dynamic"
-
-// lopobj.appendChild(newListItem);
-
 fetch("http://localhost:8080/api/v1/player")
 .then((response) => {return response.json()})
 .then((parsedResponse)=>{
     console.log(parsedResponse);
+let lopobj = document.getElementById("lop");
+
+parsedResponse.forEach(element =>{
+    let newListItem = document.createElement("li");
+    newListItem.textContent = element.name;
+    lopobj.appendChild(newListItem);
+
+});
+
 })
