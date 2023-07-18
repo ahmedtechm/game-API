1e13553e969e8b0bf2077e543d20688a622fb8a8
ahmedtechm <ahmedtechm2050@gmail.com>
2023-07-18 12:30:27 +0400

    Added JavaScript code to fetch players from the server

--- game-ui/dataFetcher.js
+++ game-ui/dataFetcher.js
@@ -1,6 +1,12 @@
 let lopobj = document.getElementById("lop");
 
-let newListItem = document.createElement("li");
-newListItem.textContent = "Dynamic"
+// let newListItem = document.createElement("li");
+// newListItem.textContent = "Dynamic"
 
-lopobj.appendChild(newListItem);
+// lopobj.appendChild(newListItem);
+
+fetch("http://localhost:8080/api/v1/player")
+.then((response) => {return response.json()})
+.then((parsedResponse)=>{
+    console.log(parsedResponse);
+})
