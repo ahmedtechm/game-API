e5fa66fc6526859990c3ee1d8807eec68a297b80
ahmedtechm <ahmedtechm2050@gmail.com>
2023-07-23 17:01:15 +0400

    Add functions for adding, updating, and deleting players

--- game-ui/dataFetcher.js
+++ game-ui/dataFetcher.js
@@ -1,28 +1,112 @@
-fetch("http://localhost:8080/api/v1/player")
-.then((response) => {return response.json()})
-.then((parsedResponse)=>{
-    console.log(parsedResponse);
-    let lopobj = document.getElementById("lop");
+// dataFetcher.js
+document.addEventListener("DOMContentLoaded", function () {
+    fetchPlayers();
+});
 
+function fetchPlayers() {
+    fetch("http://localhost:8080/api/v1/player")
+        .then((response) => response.json())
+        .then((parsedResponse) => {
+            console.log(parsedResponse);
+            let lopObj = document.getElementById("lop");
+            lopObj.innerHTML = ""; // Clear the list before adding updated players
 
-parsedResponse.forEach(element =>{
-    let newListItem = document.createElement("li"); 
-    newListItem.className = "list-group-item";
+            parsedResponse.forEach((element) => {
+                let newListItem = document.createElement("li");
+                newListItem.textContent = element.id + " -- " + element.name;
+                lopObj.appendChild(newListItem);
+            });
+        })
+        .catch((error) => console.log("error", error));
+}
 
-let listItemHeading = document.createElement("h3");
-listItemHeading.className = "card-title";
-listItemHeading.textContent = "ID: " + element.id;
+function addPlayer() {
+    let playerId = document.getElementById("playerId").value;
+    let playerName = document.getElementById("playerName").value;
 
-let listItemPara = document.createElement("p");
-listItemPara.className = "card-description";
-listItemPara.textContent = "Name: " + element.name;
+    console.log(playerId);
+    console.log(playerName);
 
-newListItem.appendChild(listItemHeading);
-newListItem.appendChild(listItemPara);
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
 
-lopobj.appendChild(newListItem);
-
-
+    var raw = JSON.stringify({
+        id: playerId,
+        name: playerName,
     });
 
-})
+    var requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+    };
+
+    fetch("http://localhost:8080/api/v1/player", requestOptions)
+        .then((response) => response.text())
+        .then((result) => {
+            console.log(result);
+            fetchPlayers(); // Update the player list after successful addition
+        })
+        .catch((error) => console.log("error", error));
+}
+
+function updatePlayer() {
+    let playerId = document.getElementById("updatePlayerId").value;
+    let playerName = document.getElementById("updatePlayerName").value;
+
+    console.log(playerId);
+    console.log(playerName);
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+        name: playerName,
+    });
+
+    var requestOptions = {
+        method: "PUT",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+    };
+
+    fetch(`http://localhost:8080/api/v1/player/${playerId}`, requestOptions)
+        .then((response) => response.text())
+        .then((result) => {
+            console.log(result);
+            fetchPlayers(); // Update the player list after successful update
+        })
+        .catch((error) => console.log("error", error));
+}
+
+function deletePlayer() {
+    let playerId = document.getElementById("deletePlayerId").value;
+
+    console.log(playerId);
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+        id: playerId,
+    });
+
+    var requestOptions = {
+        method: "DELETE",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+    };
+
+    fetch(`http://localhost:8080/api/v1/player/${playerId}`, requestOptions)
+        .then((response) => response.text())
+        .then((result) => {
+            console.log(result);
+            fetchPlayers(); // Update the player list after successful deletion
+        })
+        .catch((error) => console.log("error", error));
+}
+
+
