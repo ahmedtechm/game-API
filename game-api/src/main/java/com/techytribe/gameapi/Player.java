064f2e15e52ce4132ca117d2f2ca3354e8912668
ahmedtechm <ahmedtechm2050@gmail.com>
2023-07-30 14:01:11 +0400

    Added database configuration properties

--- game-api/src/main/java/com/techytribe/gameapi/Player.java
+++ game-api/src/main/java/com/techytribe/gameapi/Player.java
@@ -1,10 +1,14 @@
 package com.techytribe.gameapi;
 
-import javax.persistence.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "player")
 public class Player {
-
+    @Id
     public String id;
 
     public String name;
